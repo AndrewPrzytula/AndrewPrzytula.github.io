2fb1aed7bce804ab7f9e89d8f6e9cce0c435c2e4
Andrzej <and.przytula@gmail.com>
2020-07-10 13:32:44 -0500

    first commit

---
+++ precache-manifest.2a4cbb9c8b8867aeb7c4ded40f513f73.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5358ae7d5ff335e9d00bc55cfc094813",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d60492ed1c1c945da8bc",
+    "url": "/static/js/2.d14efbbc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.d14efbbc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "77b2b5ee3c3696cb7c7f",
+    "url": "/static/js/main.cd2c8c70.chunk.js"
+  },
+  {
+    "revision": "73a4f215652a07459b34",
+    "url": "/static/js/runtime-main.3bbf3189.js"
+  }
+]);
